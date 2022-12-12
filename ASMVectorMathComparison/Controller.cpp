f7b850fdad8e36b8e5f05c490a84073b128cd7a1
Lucas Spiker <lucas.spiker@mymail.champlain.edu>
2022-12-11 19:25:00 -0500

    Finalized runtime testing.

--- ASMVectorMathComparison/Controller.cpp
+++ ASMVectorMathComparison/Controller.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <chrono>
+#include <stdlib.h>
 
 void PrintVector(Vector3 v)
 {
@@ -57,11 +58,8 @@ void TestAllFunctionsAndPrint()
 	std::cout << "v1 == v2: " << (v1 == v2) << " v1 != v2: " << (v1 != v2) << std::endl;
 }
 
-void TestAllFunctions()
+void volatile TestAllFunctions(Vector3& v1, Vector3& v2)
 {
-	Vector3 v1 = Vector3(1, 2, 3);
-	Vector3 v2 = Vector3(4, 5, 6);
-
 	Vector3 v3 = v1.Normal();
 	Vector3 v4 = v2;
 
@@ -87,15 +85,36 @@ void TestAllFunctions()
 
 int main()
 {
-	std::chrono::steady_clock::time_point start = std::chrono::high_resolution_clock::now();
-	
+	std::chrono::high_resolution_clock::time_point now = std::chrono::high_resolution_clock::now();
+	long long seed = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+	srand(seed);
 
-	for (int i = 0; i < 10000000; i++)
+	Vector3* v1arr = new Vector3[1000000];
+	Vector3* v2arr = new Vector3[1000000];
+
+	std::cout << "Generating random vectors..." << std::endl;
+
+	for (int i = 0; i < 1000000; i++)
 	{
-		TestAllFunctions();
+		v1arr[i].x = rand() / RAND_MAX * 10000.0f;
+		v1arr[i].y = rand() / RAND_MAX * 10000.0f;
+		v1arr[i].z = rand() / RAND_MAX * 10000.0f;
+
+		v2arr[i].x = rand() / RAND_MAX * 10000.0f;
+		v2arr[i].y = rand() / RAND_MAX * 10000.0f;
+		v2arr[i].z = rand() / RAND_MAX * 10000.0f;
 	}
 
-	std::chrono::steady_clock::time_point done = std::chrono::high_resolution_clock::now();
+	std::cout << "Running tests..." << std::endl;
+
+	std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+	
+	for (int i = 0; i < 1000000; i++)
+	{
+		TestAllFunctions(v1arr[i], v2arr[i]);
+	}
+
+	std::chrono::high_resolution_clock::time_point done = std::chrono::high_resolution_clock::now();
 
 	std::cout << std::endl << "Took: " << std::chrono::duration_cast<std::chrono::milliseconds>(done - start).count() << std::endl;
 
